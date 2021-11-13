e765ec38b9c18a13512d9aee514f8d099dbdc95e
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-11-13 02:17:23 +0200

    modified page & added icons

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const homeBtn = document.querySelector(".home-btn");
+const workingBtn = document.querySelector(".working-btn");
+const educationBtn = document.querySelector(".education-btn");
+const skilsBtn = document.querySelector(".skils-btn");
+
+const homeInfo = document.querySelector(".home-info");
+const workingInfo = document.querySelector(".working-info");
+const educationInfo = document.querySelector(".education-info");
+const skilsInfo = document.querySelector(".skils-info");
+
+homeBtn.addEventListener("click", () => {
+   homeInfo.style.display = "flex";
+   workingInfo.style.display = "none";
+   educationInfo.style.display = "none";
+   skilsInfo.style.display = "none";
+});
+
+workingBtn.addEventListener("click", () => {
+   homeInfo.style.display = "none";
+   workingInfo.style.display = "block";
+   educationInfo.style.display = "none";
+   skilsInfo.style.display = "none";
+});
+
+educationBtn.addEventListener("click", () => {
+   homeInfo.style.display = "none";
+   workingInfo.style.display = "none";
+   educationInfo.style.display = "block";
+   skilsInfo.style.display = "none";
+});
+
+skilsBtn.addEventListener("click", () => {
+   homeInfo.style.display = "none";
+   workingInfo.style.display = "none";
+   educationInfo.style.display = "none";
+   skilsInfo.style.display = "flex";
+});
